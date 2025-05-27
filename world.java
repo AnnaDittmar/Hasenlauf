3f5b253f5786dc66f6f6e835e63e68030a0c6151
anlu <anna-maria.dittmar@gmx.de>
2025-05-27 09:20:49 +0200

    Klasse erstellt

---
+++ world.java
@@ -0,0 +1,22 @@
+
+/**
+ * Tolle Klasse die hoffentlich viel kann
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class world
+{
+    // Attribute
+
+    
+    public world()
+    {
+    }
+
+    // weitere Methoden
+
+
+
+
+}
